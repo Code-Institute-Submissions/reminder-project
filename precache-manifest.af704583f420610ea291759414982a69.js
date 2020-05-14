97730d332a7946c26248ae54bd32deba25c043ec
Zylashro <dominik.jug@gmail.com>
2020-05-14 22:08:10 +0200

    Updates

---
+++ precache-manifest.af704583f420610ea291759414982a69.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "29126d7eac197ff601f29bc05139520e",
+    "url": "/reminder-project/index.html"
+  },
+  {
+    "revision": "8595695ef343e0afa05d",
+    "url": "/reminder-project/static/css/2.ecffae5e.chunk.css"
+  },
+  {
+    "revision": "7a4ca84e6d3ef7a5a650",
+    "url": "/reminder-project/static/css/main.6ef93dee.chunk.css"
+  },
+  {
+    "revision": "8595695ef343e0afa05d",
+    "url": "/reminder-project/static/js/2.d5fee2af.chunk.js"
+  },
+  {
+    "revision": "3a5359d431b8190dc2f36e4703f39768",
+    "url": "/reminder-project/static/js/2.d5fee2af.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7a4ca84e6d3ef7a5a650",
+    "url": "/reminder-project/static/js/main.bfba02bf.chunk.js"
+  },
+  {
+    "revision": "91f6ce202ad08139328e",
+    "url": "/reminder-project/static/js/runtime-main.f799ea5f.js"
+  }
+]);
