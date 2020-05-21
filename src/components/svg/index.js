ed8614babcc277d3ea4d0aef71447e3dfb153b40
Zylashro <dominik.jug@gmail.com>
2020-05-21 17:01:27 +0200

    The how to use section styles separated to own file. Added general styles for section.

--- src/components/svg/index.js
+++ src/components/svg/index.js
@@ -4,5 +4,20 @@ import InfoCircle from "./InfoCircle";
 import Reminder from "./Reminder";
 import Moon from "./Moon";
 import Sun from "./Sun";
+import Up from "./Up";
+import Down from "./Down";
+import Close from "./Close";
+import Complete from "./Complete";
 
-export { Logo, AlarmClock, InfoCircle, Reminder, Moon, Sun };
+export {
+	Logo,
+	AlarmClock,
+	InfoCircle,
+	Reminder,
+	Moon,
+	Sun,
+	Up,
+	Down,
+	Close,
+	Complete,
+};
