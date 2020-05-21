99beb3b0d9a2186347e296739f453b0f1c7223d6
Zylashro <dominik.jug@gmail.com>
2020-05-21 15:43:54 +0200

    Updated header icons, excluded styles to separate file, top-bar replaced with nav-bar.

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,46 +1,55 @@
-import React from 'react';
-import PropTypes from 'prop-types';
-import Logo from './Logo';
-import { FaMoon, FaSun } from 'react-icons/fa';
-import { MdAddAlarm } from 'react-icons/md';
-import { BsInfoCircle } from 'react-icons/bs';
+import React from "react";
+import PropTypes from "prop-types";
+import "./Header.css";
+import { Logo, AlarmClock, InfoCircle, Reminder, Moon, Sun } from "./svg";
 
 const Header = (props) => {
-	const iconCode = () => {
-		if (props.isDark) {
-			return <FaSun id='icon' className='icon' alt='Change into dark mode button' />;
-		}
-		return <FaMoon id='icon' className='icon' alt='Change into light mode button' />;
-	};
-
 	return (
-		<header className='header-background'>
-			<div className='logo-container'>
+		<header className="header">
+			<div className="header__logo-container">
 				<Logo />
 			</div>
-			<div className='top-bar-container'>
-				<ul className='top-bar'>
-					<li
-						className='li-item first-item start-app'
-						onClick={() => props.handleForm(true)}
-					>
-						<MdAddAlarm
-							className='icon'
-							alt='Add reminder button'
+			<div className="header__nav-container">
+				<ul className="header__nav-bar">
+					<li className="header__nav-item">
+						<AlarmClock
+							holderClass="header__icon-holder"
+							iconClass="header__icon"
+							alt="Go to add reminder section"
+						/>
+					</li>
+					<li className="header__nav-item">
+						<InfoCircle
+							holderClass="header__icon-holder"
+							iconClass="header__icon"
+							alt="Go to how to use section"
+						/>
+					</li>
+					<li className="header__nav-item">
+						<Reminder
+							holderClass="header__icon-holder"
+							iconClass="header__icon"
+							alt="Go to list section"
 						/>
 					</li>
 					<li
-						className='li-item'
-						onClick={() => props.handleVisibility(!props.isHidden)}
-					>
-						<BsInfoCircle className='icon' />
-					</li>
-					<li
-						id='theme-id'
-						className='li-item'
+						className="header__nav-item"
 						onClick={() => props.handleTheme(!props.isDark)}
 					>
-						{iconCode()}
+						{props.isDark && (
+							<Sun
+								holderClass="header__icon-holder"
+								iconClass="header__icon"
+								alt="Change into light mode"
+							/>
+						)}
+						{!props.isDark && (
+							<Moon
+								holderClass="header__icon-holder"
+								iconClass="header__icon"
+								alt="Change into dark mode"
+							/>
+						)}
 					</li>
 				</ul>
 			</div>
@@ -51,7 +60,6 @@ const Header = (props) => {
 Header.propTypes = {
 	isDark: PropTypes.bool.isRequired,
 	handleTheme: PropTypes.func.isRequired,
-	handleForm: PropTypes.func.isRequired
 };
 
 export default Header;
---
+++ src/components/svg/index.js
@@ -0,0 +1,8 @@
+import Logo from "./Logo";
+import AlarmClock from "./AlarmClock";
+import InfoCircle from "./InfoCircle";
+import Reminder from "./Reminder";
+import Moon from "./Moon";
+import Sun from "./Sun";
+
+export { Logo, AlarmClock, InfoCircle, Reminder, Moon, Sun };
